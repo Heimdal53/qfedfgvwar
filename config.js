696e339685c68efde05bb9ae8b0db39f460c0b27
Heimdal <104633853+Heimdal53@users.noreply.github.com>
2025-01-16 02:59:16 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const API_URL = "https://api.openai.com/v1/chat/completions";
+const strIndex = "";
+const system_message = "Merhaba Ben MavorAI! Nasıl yardımcı olabilirim size?"; // You can change this message
+const path = "/chat";  // Change path to your path. For localhost make it path = "";
+// const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
---
+++ readme.md
@@ -0,0 +1,7 @@
+# kurulum
+
+- npm install yazarak gerekli modülleri yükleyin
+
+- config.js dosyasındaki gereken yerleri doldurarak kullanıma başlayabilirsiniz.
+
+# (https://github.com/Lazutti)
